0e55f5efc09b490aa45f1cea84256281a6f07d9b
lainestein <laine163@163.com>
2015-08-26 19:31:11 +0800

    Merge branch 'master' of https://github.com/miniPinocchio/XLCeremony
    Conflicts:
    	.idea/misc.xml
    	app/app.iml
    
    25晚pull的

---
+++ app/src/main/java/com/liuhui/xlceremony/app/bean/recPlan.java
@@ -0,0 +1,160 @@
+package com.liuhui.xlceremony.app.bean;
+
+/**
+ * Created by Administrator on 2015/8/26.
+ */
+public class recPlan {
+    private int id;
+    private String title;
+    private String content;
+    private String fengmian;
+    private int userid;
+    private String nickname;
+    private String acatar;
+    private int praisenum;
+    private int commentnum;
+    private int hits;
+    private int share;
+    private String keywords;
+    private String gifturl;
+    private String showgift;
+    private String updatetime;
+    private int typeid;
+    private String typename;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFengmian() {
+        return fengmian;
+    }
+
+    public void setFengmian(String fengmian) {
+        this.fengmian = fengmian;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getAcatar() {
+        return acatar;
+    }
+
+    public void setAcatar(String acatar) {
+        this.acatar = acatar;
+    }
+
+    public int getPraisenum() {
+        return praisenum;
+    }
+
+    public void setPraisenum(int praisenum) {
+        this.praisenum = praisenum;
+    }
+
+    public int getCommentnum() {
+        return commentnum;
+    }
+
+    public void setCommentnum(int commentnum) {
+        this.commentnum = commentnum;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public void setHits(int hits) {
+        this.hits = hits;
+    }
+
+    public int getShare() {
+        return share;
+    }
+
+    public void setShare(int share) {
+        this.share = share;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getGifturl() {
+        return gifturl;
+    }
+
+    public void setGifturl(String gifturl) {
+        this.gifturl = gifturl;
+    }
+
+    public String getShowgift() {
+        return showgift;
+    }
+
+    public void setShowgift(String showgift) {
+        this.showgift = showgift;
+    }
+
+    public String getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(String updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public int getTypeid() {
+        return typeid;
+    }
+
+    public void setTypeid(int typeid) {
+        this.typeid = typeid;
+    }
+
+    public String getTypename() {
+        return typename;
+    }
+
+    public void setTypename(String typename) {
+        this.typename = typename;
+    }
+}
